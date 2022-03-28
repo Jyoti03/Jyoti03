c2084ef667120acdcc4c8deb2dc4ceda7b1b0de6
Jyoti03 <35188270+Jyoti03@users.noreply.github.com>
2022-03-28 16:41:03 +0530

    Add files via upload
    A Department Microservice and inserting a record using post.

---
+++ Microservice_Department/src/main/java/com/example/demo/entity/Department.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Department {
+	
+	private Long DepartmentId;
+	private String DepatmentName;
+	private String DepartmentAddress;
+	private String Departmentcode;
+	
+
+}
